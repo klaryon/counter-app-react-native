e82169415f2f28316a18b3d202afe8831259305d
klaryon <klary.ardon@gmail.com>
2022-05-12 13:11:00 +0200

    touchableOpacity buttons styled

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -7,9 +7,18 @@ export const CounterScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Counter: {contador}</Text>
-      <TouchableOpacity onPress={() => setContador(contador + 1)}>
-        <View style={styles.buttonIncrement}>
-          <Text>+1</Text>
+      <TouchableOpacity
+        style={styles.fabLocationBR}
+        onPress={() => setContador(contador + 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setContador(contador - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -26,8 +35,27 @@ const styles = StyleSheet.create({
     fontSize: 40,
     top: -15,
   },
-  buttonIncrement: {
-    backgroundColor: 'salmon',
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
     borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
 });
