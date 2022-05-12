5310c5b7f1732da5aa41af2485419b9cc05fbb0d
klaryon <klary.ardon@gmail.com>
2022-05-12 13:36:42 +0200

    Fab component created

---
+++ src/components/Fab.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+interface Props {
+  title: string;
+}
+
+export const Fab = ({title}: Props) => {
+  console.log(title);
+  return (
+    <TouchableOpacity
+      style={styles.fabLocationBR}
+      onPress={() => console.log('Click')}>
+      <View style={styles.fab}>
+        <Text style={styles.fabText}>+1</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
