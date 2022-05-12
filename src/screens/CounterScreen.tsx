2fdc641b15cb2af6a4028e028183bf926b16ae60
klaryon <klary.ardon@gmail.com>
2022-05-12 12:48:29 +0200

    StyleSheet added

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,19 +1,33 @@
 import React, {useState} from 'react';
-import {Button, Text, TouchableOpacity, View} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 export const CounterScreen = () => {
   const [contador, setContador] = useState(10);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text style={{textAlign: 'center', fontSize: 40, top: -15}}>
-        Counter: {contador}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Counter: {contador}</Text>
       <TouchableOpacity onPress={() => setContador(contador + 1)}>
-        <View style={{backgroundColor: 'salmon', borderRadius: 100}}>
-          <Text>Click Me!</Text>
+        <View style={styles.buttonIncrement}>
+          <Text>+1</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15,
+  },
+  buttonIncrement: {
+    backgroundColor: 'salmon',
+    borderRadius: 100,
+  },
+});
