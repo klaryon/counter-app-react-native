1677d92cad1cf19d8aa2bc4fe4e5bc0a359a3881
klaryon <klary.ardon@gmail.com>
2022-05-13 08:34:27 +0200

    justifyContent and alignItems examples

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -15,21 +15,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
   },
   box1: {
-    flex: 3, // 3 + 2 + 2 = 6
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   box2: {
-    flex: 2, // 3 + 2 + 2 = 6
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   box3: {
-    flex: 1,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
