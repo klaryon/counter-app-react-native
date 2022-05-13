5889d34ee48da332bd0b616873cbc6847f9dbc6a
klaryon <klary.ardon@gmail.com>
2022-05-13 08:42:19 +0200

    align self example

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -16,22 +16,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#28C4D9',
     flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
   },
   box1: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
+    alignSelf: 'center',
   },
   box2: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
+    alignSelf: 'flex-start',
   },
   box3: {
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
+    alignSelf: 'flex-end',
   },
 });
